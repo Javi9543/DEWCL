2b2fb9a717d3633b23b5dbf7baef1c0ec1b85680
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2026-09-24 09:50:07 +0200

    Preparo la estructura de archivos de la relacion 1-3 y hago el ejercicio1

---
+++ UD1/Ejercicios_UD1/Ejercicios_Relacion3/relacion1-3.js
@@ -0,0 +1,13 @@
+//Ejercicio 1
+    function calcularEdad(edad1, edad2){
+        let resultado1 = edad1 - edad2;
+        let resultado2 = resultado1 - 1;
+
+        return "La edad de la persona está entre, " + resultado1 + " y " + resultado2;
+
+    }
+//Ejercicio 2
+
+//Ejercicio 3
+
+//Ejercicio 4
